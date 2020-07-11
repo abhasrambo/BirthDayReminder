843a8a299a10e20fdf18d928f6ac1fd401ace3dc
Abhas Kumar <abhasroy@gmail.com>
2020-07-10 22:44:12 -0700

    Finished local persistence

--- BirthDayReminder/ViewController.swift
+++ BirthDayReminder/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let nameStored = UserDefaults.standard.object(forKey: "name")
+        let birthdayStored = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let name = nameStored as? String, let birthday = birthdayStored as? String {
+            nameLabel.text = name
+            birthdayLabel.text = birthday
+        }
         // Do any additional setup after loading the view.
     }
+    
+    func savedata() {
+        if let nameText = nameTextField.text {
+            UserDefaults.standard.set(nameText, forKey: "name")
+            nameLabel.text = "Name: \(nameText)"
+        }
+        if let birthdayText = birthdayTextField.text {
+            UserDefaults.standard.set(birthdayText, forKey: "birthday")
+            birthdayLabel.text = "Birthday: \(birthdayText)"
+        }
+    }
 
-
+    @IBAction func saveDetails(_ sender: UIButton) {
+        savedata()
+    }
+    
 }
 
